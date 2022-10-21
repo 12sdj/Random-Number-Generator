d13fa1508518fcdb9f5dbbbe9506430d629cd355
12sdj <103876733+12sdj@users.noreply.github.com>
2022-10-21 19:37:38 +0800

    Create Random 02.py

---
+++ Beta Version/Version 1.0.1_Alpha 02/Random 02.py
@@ -0,0 +1,8 @@
+#Version 1.0.1_Alpha02
+from random import*
+begin = int(input("Please enter a smaller value: "))
+end = int(input("Please enter a larger value: "))
+while(True):
+    number = randint(begin,end)
+    print(number)
+    number = 0
